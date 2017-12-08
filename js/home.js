c032158c087de11a366067977baa5d9460352690
Dalia Mohamed <dalia.mohamed426@yahoo.co.uk>
2017-12-08 18:52:11 +0200

    updated

--- js/home.js
+++ js/home.js
@@ -1,14 +1,14 @@
-//$(function() {
-    // new WOW().init(); // wow.js for scrolling animation
+$(function() {
+    new WOW().init(); // wow.js for scrolling animation
 
- // scroll to sections
+ //scroll to sections
 
-//  $(".navbar .navbar-nav a.nav-link").on("click", function() {
-//     var object = $(this);
-//     $("html ,body").animate({
-//         scrollTop: $("#" + object.data("scroll") + "-section").offset().top - 55
-//     }, 700);
-// });
+ $(".navbar .navbar-nav a.nav-link").on("click", function() {
+    var object = $(this);
+    $("html ,body").animate({
+        scrollTop: $("#" + object.data("scroll") + "-section").offset().top - 55
+    }, 700);
+});
 
 
-//});
+});
