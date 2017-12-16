16aaa2f687b236228ae4e79f5d880c6c4c9cb60f
Mary_N <marynader256@gmail.com>
2017-12-16 12:09:34 +0200

    design

---
+++ RestCategories/Dessert.php
@@ -0,0 +1,55 @@
+<?php
+
+    // page title
+    $pageTitle = 'Dessert';
+
+    // css files for this page
+    $css_files = '<link rel="stylesheet" href="../css/RestCateg/Dessert.css">';
+
+    include '../init.php';
+?>
+
+<div class="overlay">
+  <div class="list-group" >
+  <a href="#" class="list-group-item " >
+  <h4 class="list-group-item-heading " >Mac</h4>
+  <p class="list-group-item-text">4are3 9</p>
+  </a>
+  <a href="#" class="list-group-item">
+  <h4 class="list-group-item-heading" >KFC</h4>
+  <p class="list-group-item-text">4are3 9</p>
+  </a>
+  <a href="#" class="list-group-item">
+  <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+  <p class="list-group-item-text">List Group Item Text</p>
+  </a>
+  <a href="#" class="list-group-item">
+  <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+  <p class="list-group-item-text">List Group Item Text</p>
+  </a>
+  <a href="#" class="list-group-item">
+  <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+  <p class="list-group-item-text">List Group Item Text</p>
+  </a>
+  <a href="#" class="list-group-item">
+  <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+  <p class="list-group-item-text">List Group Item Text</p>
+  </a>
+  <a href="#" class="list-group-item">
+  <h4 class="list-group-item-heading">Third List Group Item Heading</h4>
+  <p class="list-group-item-text">List Group Item Text</p>
+  </a>
+  
+  </div>
+  </div>
+
+
+
+
+
+<?php 
+    // $js_files = '<script src="../js/restaurants/restaurants.js"></script>';
+
+    include '../' . $tmpl . 'footer.php';
+
+?>       
